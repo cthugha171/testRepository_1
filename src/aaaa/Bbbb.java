0ae81dddfdaac3eb10685830d615a37ca5d491a4
sato tatsuya <tatsu.sato0519@gmail.com>
2025-01-30 09:35:10 +0900

    マージ

--- src/aaaa/Bbbb.java
+++ src/aaaa/Bbbb.java
@@ -2,4 +2,14 @@ package aaaa;
 
 public class Bbbb extends Aaaa {
 
+	public Bbbb(String nameValue, Integer ageValue) {
+		super();
+		name = nameValue;
+		age = ageValue;
+	}
+	
+	public void introduction() {
+		String intro = "私は"+ name + "です" + "\r\n 年齢は" + age + "です";
+		System.out.print(intro);
+	}
 }
